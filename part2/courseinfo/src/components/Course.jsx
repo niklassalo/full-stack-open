b9c64d87b4436237707702e46fcb5774929bb951
Niklas Salo <niklas.salo@hotmail.com>
2023-10-05 00:59:54 +0300

    destructurin

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,36 +1,26 @@
 const Header = ({ courseName }) => {
-    return (
-        <div>
-            <h2>{courseName}</h2>
-        </div>
-    )
+    return <h2>{courseName}</h2>
 }
 
 const Part = ({ part }) => {
-    return (
-        <p>{part.name} {part.exercises}</p>
-    )
+    return <p>{part.name} {part.exercises}</p>
 }
 
 const Content = ({ parts }) => {
     return (
         <div>
-            {parts.map(part =>
-            <Part key={part.id} part={part} />
-            )}
+            {parts.map(part =><Part key={part.id} part={part} />)}
         </div>
     )
 }
 
-const Total = (props) => {
-    const totalExercises = props.parts.reduce((sum, part) => sum + part.exercises, 0)
-    return (
-        <p>Total of {totalExercises} exercises</p>
-    )
+const Total = ({ parts }) => {
+    const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0)
+    return <p>Total of {totalExercises} exercises</p>
 }
 
 const Course = ({ course }) => {
-    return(
+    return (
         <div>
             <Header courseName={course.name} />
             <Content parts={course.parts} />
